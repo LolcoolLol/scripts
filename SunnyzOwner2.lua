02c0d86ab0dec74b1555cbd678fd318188097b56
LolcoolLol <124131964+LolcoolLol@users.noreply.github.com>
2023-05-31 20:58:51 +0800

    Update SunnyzOwner2.lua

--- SunnyzOwner2.lua
+++ SunnyzOwner2.lua
@@ -1,5 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LolcoolLol/scripts/main/vape.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
 task.wait(0.12)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LolcoolLol/scripts/main/cool.lua", true))()
+task.wait(0.1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LolcoolLol/scripts/main/cool.lua", true))()
 -- Dnut config
 local loaded
