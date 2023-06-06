f03d1c0a6a5a78383fe91555e8918e425083d688
LolcoolLol <124131964+LolcoolLol@users.noreply.github.com>
2023-06-06 13:49:17 +0800

    Update SunnyzOwnerFIX.lua

--- SunnyzOwnerFIX.lua
+++ SunnyzOwnerFIX.lua
@@ -2,6 +2,8 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4Fo
 task.wait(0.12) 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LolcoolLol/scripts/main/cool.lua", true))() 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LolcoolLol/scripts/main/cool.lua", true))()
+-- version
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LolcoolLol/scripts/main/version.lua", true))()
 -- Dnut config
 local loaded
 repeat task.wait() until game:IsLoaded()
